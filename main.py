5d741c3e46e65980551d72cdf8f1054e7dd3ea3e
Mohsen <moh3en.mn.1658@gmail.com>
2025-09-26 17:43:34 +0330

    update main for printing Ali Hejazizo

--- main.py
+++ main.py
@@ -3,10 +3,7 @@ from utils import add
 
 print("Hello world")
 def main():
-    a = 10
-    b = 20
-    c = add(a, b)
-    print(c)
+    print('Ali Hejazizo')
 
 if __name__ == '__main__':
     main()
